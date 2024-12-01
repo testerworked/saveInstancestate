c074ccdc2238f55545755144d4f055769423055b
Rinat Khuzin <khuzin.rinat.work@gmail.com>
2024-12-01 14:42:39 +0500

    add: save instance state sample;

--- app/src/main/java/com/state/saveinstancestate/MainActivity.kt
+++ app/src/main/java/com/state/saveinstancestate/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.state.saveinstancestate
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var messageTV: TextView
+    private lateinit var inputET : EditText
+    private lateinit var buttonBTN: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        messageTV = findViewById(R.id.outMessage)
+        inputET = findViewById(R.id.inputET)
+        buttonBTN = findViewById(R.id.inputB)
+
+        buttonBTN.setOnClickListener{
+            messageTV.text = inputET.text
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState?.run {
+            putString("key", messageTV.text.toString())
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        messageTV.text = savedInstanceState.getString("key")
     }
 }
